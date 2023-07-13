296b514ce125d7dd34a9446cf1df09e1047b978a
sr_sumanroy <sumantachtg@gmail.com>
2023-07-13 06:11:33 -0500

    first commit

---
+++ .vscode/bin/input.dart
@@ -0,0 +1,14 @@
+// import 'dart:io';
+
+// void main() {
+//   String? name = stdin.readLineSync();
+//   print("Enter your First name : ");
+//   String? last = stdin.readLineSync();
+//   print("Enter your Last Name  : ");
+//   // String fullname = name + last;
+//   print("Enter your  Name  :${name} ");
+// }
+
+void main(){
+  print("Hello Bangladesh");
+}
---
+++ .vscode/bin/list.dart
@@ -0,0 +1,8 @@
+import 'dart:io';
+
+void main() {
+  print("Hello world");
+  stdout.writeln("first number : ");
+String a = int.parse(stdin.readLineSync());
+  print("first number :$a");
+}
