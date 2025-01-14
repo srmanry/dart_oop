e74939e4fb71d8f0c9c8198e9e27c2c59b3be297
srmanry <sumantachtg@gmail.com>
2025-01-14 04:10:09 +0300

    add dataType

---
+++ .vscode/bin/date__type.dart
@@ -0,0 +1,28 @@
+
+
+// Dart Data type  Class  , ID = 10061;
+void main(){
+
+String topic = "Dart Data type  Class";
+String batch  = "Batch 11";
+
+print("Topic Leangth is  =  ${topic.length}");
+int id =10061;
+print("Class Topic =$topic" );
+print("Bacth = $batch");
+print("Id = $id");
+
+bool a = false;
+  print(a); //false
+bool b = a;
+print("result b ==${b}"); //true
+
+double num1 = 2.78;
+double num2 = 34.7;
+double result =num2/num1;
+
+print(result);
+print(result.toStringAsFixed(4));
+print(result.toInt());
+
+}
